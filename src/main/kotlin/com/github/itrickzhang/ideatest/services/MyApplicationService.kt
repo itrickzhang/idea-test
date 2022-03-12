367c20a9c5e2f6ec054c990c447a7f95b65838eb
GitHub Action <action@github.com>
2022-03-12 08:01:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/itrickzhang/ideatest/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/itrickzhang/ideatest/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.itrickzhang.ideatest.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.itrickzhang.ideatest.MyBundle
 
 class MyApplicationService {
 
--- src/test/kotlin/com/github/itrickzhang/ideatest/MyPluginTest.kt
+++ src/test/kotlin/com/github/itrickzhang/ideatest/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.itrickzhang.ideatest
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
